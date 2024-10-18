1a417889a7be09fe3f7b26a1afb5cbb59e62ed3a
Avishka Pasindu <avishkap@mitesp.com>
2024-10-18 20:10:05 +0530

    added docker compose

---
+++ Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Core.Commands;
+using Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task sendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ Domain.Core/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
